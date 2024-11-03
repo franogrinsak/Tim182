3a11eca937f850471fd5914eef76e4e80d4103fe
pp19991 <patrik.pasic@fer.hr>
2024-11-03 20:10:56 +0100

    dodaj

--- Develop/backend/src/main/java/com/primjer/primjer/SecurityConfig.java
+++ Develop/backend/src/main/java/com/primjer/primjer/SecurityConfig.java
@@ -1,18 +1,26 @@
 package com.primjer.primjer;
 
-import org.springframework.boot.autoconfigure.batch.BatchDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     private final AuthSuccessHandler authHandler;
-
+    private String frontendUrl="http://localhost:5137";
     public SecurityConfig(AuthSuccessHandler authHandler) {
         this.authHandler = authHandler;
     }
@@ -20,11 +28,28 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
-        return http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth->{
-            auth.requestMatchers("/","/login").permitAll();
-            auth.anyRequest().authenticated();
-        }).oauth2Login(oauth2 -> oauth2
-                .successHandler(authHandler)).build();
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(auth -> {
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Login(oath2 -> {
+                    oath2.loginPage("/login").permitAll();
+                    oath2.successHandler(authHandler);
+                })
+                .build();
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(frontendUrl));
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
+        return urlBasedCorsConfigurationSource;
     }
 
 }

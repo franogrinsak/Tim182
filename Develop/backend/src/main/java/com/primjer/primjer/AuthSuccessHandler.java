8e5d97e78539a01e3ec2117677ac63b51ce07114
pp19991 <patrik.pasic@fer.hr>
2024-11-03 15:40:18 +0100

    ispravi

--- Develop/backend/src/main/java/com/primjer/primjer/AuthSuccessHandler.java
+++ Develop/backend/src/main/java/com/primjer/primjer/AuthSuccessHandler.java
@@ -26,7 +26,7 @@ public class AuthSuccessHandler implements AuthenticationSuccessHandler {
             user.setEmail(email);
             userRepo.storeUser(user);
         }
-        response.sendRedirect("/api/logged");
+        response.sendRedirect("https://www.google.com/");
     }
 
 }

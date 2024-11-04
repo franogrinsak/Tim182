b58f24bdd986c17a7b9f2b1656eb21aad4e2ffa7
pp19991 <patrik.pasic@fer.hr>
2024-11-04 17:27:47 +0100

    Ispravi

--- Develop/backend/src/main/java/com/primjer/primjer/SecurityConfig.java
+++ Develop/backend/src/main/java/com/primjer/primjer/SecurityConfig.java
@@ -29,12 +29,12 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
         return http.csrf(AbstractHttpConfigurer::disable).cors(cors -> cors.configurationSource(corsConfigurationSource())).authorizeHttpRequests(auth->{
-            auth.requestMatchers("/","/api/login").permitAll();
+            auth.requestMatchers("/","/login").permitAll();
             auth.anyRequest().authenticated();
         }).oauth2Login(oauth2 -> oauth2
                 .successHandler(authHandler)).exceptionHandling(eh -> eh
                 .authenticationEntryPoint((req, res, authException) -> {
-                    res.sendRedirect("/api/login?error=unauthenticated");
+                    res.sendRedirect("/login");
                 })).build();
     }
 

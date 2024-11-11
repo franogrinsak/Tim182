948b3a4278cdaf0a1d96076aa19234db4b55012b
pp19991 <patrik.pasic@fer.hr>
2024-11-11 14:24:46 +0100

    zavrsi klasu Court UC-3

--- Develop/backend/src/main/java/com/primjer/primjer/Court.java
+++ Develop/backend/src/main/java/com/primjer/primjer/Court.java
@@ -1,4 +1,53 @@
 package com.primjer.primjer;
 
 public class Court {
+    private int courtId;
+    private String courtName;
+    private String location;
+    private boolean isIndoor;
+    private String image;
+    private int userId;
+    public int getCourtId() {
+        return courtId;
+    }
+
+    public void setCourtId(int courtId) {
+        this.courtId = courtId;
+    }
+    public String getCourtName() {
+        return courtName;
+    }
+
+    public void setCourtName(String courtName) {
+        this.courtName = courtName;
+    }
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public boolean isIndoor() {
+        return isIndoor;
+    }
+
+    public void setIndoor(boolean indoor) {
+        isIndoor = indoor;
+    }
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
 }

dbb3b3ff8f9c4b46291e438e35181368263c157b
pp19991 <patrik.pasic@fer.hr>
2024-11-11 15:34:58 +0100

    Zavrsi Controller i Repositroy klasu-UC3

--- Develop/backend/src/main/java/com/primjer/primjer/CourtController.java
+++ Develop/backend/src/main/java/com/primjer/primjer/CourtController.java
@@ -1,5 +1,37 @@
 package com.primjer.primjer;
 
+
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class CourtController {
+    private CourtRepository courtRepo;
+
+    public CourtController(CourtRepository courtRepo) {
+        this.courtRepo = courtRepo;
+    }
+    @PostMapping("/courts/add")
+    public void courtAdd(@RequestBody Court court){
+        courtRepo.addCourt(court);
+    }
+    @GetMapping("/courts")
+    public List<Court> getCourts(@RequestParam int userId){
+        return courtRepo.getCourts(userId);
+    }
+    @GetMapping("/courts/all")
+    public List<Court> getAllCourts(){
+        return courtRepo.getAllCourts();
+    }
+    @GetMapping("/courts/court")
+    public Court getCourt(@RequestParam int courtId){
+        return courtRepo.getCourt(courtId);
+    }
+    @PostMapping("/courts/edit")
+    public void courtEdit(@RequestBody Court court){
+        courtRepo.editCourt(court);
+    }
 
 }
--- Develop/backend/src/main/java/com/primjer/primjer/CourtRepository.java
+++ Develop/backend/src/main/java/com/primjer/primjer/CourtRepository.java
@@ -1,4 +1,75 @@
 package com.primjer.primjer;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class CourtRepository {
+    private final JdbcTemplate jdbc;
+
+    public CourtRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    public void addCourt(Court court) {
+        String querry="INSERT INTO courts(courtname,location,isindoor,image,userid) VALUES(?,?,?,?,?)";
+        jdbc.update(querry,
+                court.getCourtName(),court.getLocation(),court.isIndoor(),court.getImage(),court.getUserId());
+    }
+
+    public List<Court> getCourts(int userId) {
+        String querry="SELECT * FROM courts WHERE userid = ?";
+        RowMapper<Court> purchaseRowMapper = (r, i) -> {
+            Court rowObject = new Court();
+            rowObject.setUserId(r.getInt("userid"));
+            rowObject.setCourtId(r.getInt("courtid"));
+            rowObject.setImage(r.getString("image"));
+            rowObject.setIndoor(r.getBoolean("isindoor"));
+            rowObject.setCourtName(r.getString("courtname"));
+            rowObject.setLocation(r.getString("location"));
+            return rowObject;
+        };
+
+        return jdbc.query(querry, purchaseRowMapper,userId);
+    }
+
+    public List<Court> getAllCourts() {
+        String querry="SELECT * FROM courts";
+        RowMapper<Court> purchaseRowMapper = (r, i) -> {
+            Court rowObject = new Court();
+            rowObject.setUserId(r.getInt("userid"));
+            rowObject.setCourtId(r.getInt("courtid"));
+            rowObject.setImage(r.getString("image"));
+            rowObject.setIndoor(r.getBoolean("isindoor"));
+            rowObject.setCourtName(r.getString("courtname"));
+            rowObject.setLocation(r.getString("location"));
+            return rowObject;
+        };
+
+        return jdbc.query(querry, purchaseRowMapper);
+    }
+
+    public Court getCourt(int courtId) {
+        String querry="SELECT * FROM courts WHERE courtid = ?";
+        RowMapper<Court> purchaseRowMapper = (r, i) -> {
+            Court rowObject = new Court();
+            rowObject.setUserId(r.getInt("userid"));
+            rowObject.setCourtId(r.getInt("courtid"));
+            rowObject.setImage(r.getString("image"));
+            rowObject.setIndoor(r.getBoolean("isindoor"));
+            rowObject.setCourtName(r.getString("courtname"));
+            rowObject.setLocation(r.getString("location"));
+            return rowObject;
+        };
+
+        return jdbc.query(querry, purchaseRowMapper,courtId).get(0);
+    }
+
+    public void editCourt(Court court) {
+        String querry="UPDATE courts SET courtName= ?,location= ?,isIndoor= ?,image= ?,userId= ? WHERE courtid = ?";
+        jdbc.update(querry,court.getCourtName(),court.getLocation(),court.isIndoor(),court.getImage(),court.getUserId(),court.getCourtId());
+    }
 }

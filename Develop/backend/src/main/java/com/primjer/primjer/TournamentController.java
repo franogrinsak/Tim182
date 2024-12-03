22f15122c00d55cefa38cab2a3654e6e3c94af89
Lovro Matić <lm54265fer.hr>
2024-12-03 20:05:40 +0100

    Dodaj u organiziranje turnira #39

---
+++ Develop/backend/src/main/java/com/primjer/primjer/TournamentController.java
@@ -0,0 +1,22 @@
+package com.primjer.primjer;
+
+
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TournamentController {
+    private TournamentRepository tournamentRepo;
+
+    public TournamentController(TournamentRepository tournamentRepo) {
+        this.tournamentRepo = tournamentRepo;
+    }
+    @PostMapping("/tournaments/add")
+    public void tournamentAdd(@RequestBody Tournament tournament){
+        tournamentRepo.addTournament(tournament);
+    }
+    
+
+}
---
+++ Develop/backend/src/main/java/com/primjer/primjer/TournamentRepository.java
@@ -0,0 +1,24 @@
+package com.primjer.primjer;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class TournamentRepository {
+    private final JdbcTemplate jdbc;
+
+    public TournamentRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    public void addTournament(Tournament tournament) {
+        String querry="INSERT INTO tournaments(tournamentname,date,registrationfee,reward,playerlevel,description,isopen,userid,courtid) VALUES(?,?,?,?,?,?,?,?,?)";
+        jdbc.update(querry,
+                tournament.getTournamentName(),tournament.getDate(),tournament.getRegistrationFee(),tournament.getReward(),tournament.getPlayerLevel(),tournament.getDescription(),tournament.isOpen(),tournament.getUser().getUserId(),tournament.getCourt().getCourtId());
+    }
+
+
+}

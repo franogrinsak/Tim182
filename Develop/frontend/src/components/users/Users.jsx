8db6d2f624c3c29d6d0cb35f1a54ca475410bd85
franogrinsak <fo52394@fer.hr>
2025-01-23 02:17:11 +0100

    Popravi poravnanje tablice korisnika

--- Develop/frontend/src/components/users/Users.jsx
+++ Develop/frontend/src/components/users/Users.jsx
@@ -120,8 +120,8 @@ export default function Users() {
   const header = renderHeader();
 
   return (
-    <section>
-      <h2 className="text-4xl mb-4">All users</h2>
+    <section className="h-full">
+      <h2 className="text-4xl my-4">All users</h2>
       <Link to="add">
         <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
           Add user
